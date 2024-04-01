aecf6b83cf0974da65b21da205e2e39e476c377e
Niceplugin <niceplugin@gmail.com>
2024-04-01 20:08:26 +0900

    docs: korean github link

--- docs/.vitepress/config/ko.ts
+++ docs/.vitepress/config/ko.ts
@@ -17,7 +17,8 @@ export const ko = defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      pattern:
+        'https://github.com/niceplugin/vitepress-ko/edit/main-ko/docs/:path',
       text: 'Edit this page on GitHub'
     },
 
